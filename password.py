25cdef11acc625893b02e6fe70e03ce41c8914b8
irungumargaret <sairmargaret44@gmail.com>
2020-01-24 10:42:19 +0300

    add a class credentials

--- password.py
+++ password.py
@@ -3,3 +3,12 @@ class Users:
     Class that generates new instances of users
     """
     user_list = [] #Empty user list
+
+class Credentials:
+    """
+    Class that generates new instances of user credentials
+    """
+    credential_list = [] #Empty credential list
+    
+  
+  
