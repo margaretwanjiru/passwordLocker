5922d43b61a0de3a49d51a6751775d03140f5a4e
irungumargaret <sairmargaret44@gmail.com>
2020-01-24 10:43:37 +0300

    define the classes created

--- password.py
+++ password.py
@@ -4,11 +4,28 @@ class Users:
     """
     user_list = [] #Empty user list
 
+    def __init__(self,user_name,password):
+    
+        '''
+        __init__ method that helps us define properties for our users.
+        '''
+        self.user_name = user_name
+        self.password = password
+    
+    # 
 class Credentials:
     """
     Class that generates new instances of user credentials
     """
     credential_list = [] #Empty credential list
     
-  
-  
+    def __init__(self, user_name, account, account_username, account_password):
+    
+        '''
+        __init__ method that helps us define properties from our user credentials.
+        '''
+        self.user_name = user_name
+        self.account = account
+        self.account_username = account_username
+        self.account_password = account_password
+   
